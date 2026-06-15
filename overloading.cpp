f831798d11a8ced122fc32c02317f6cfe1ccd539
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-14 20:46:16 -0500

    complex numbers
    create a complex number class

--- overloading.cpp
+++ overloading.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
+class comp {
+    public: 
+    int real;
+    int im; //a +bi
 
+    comp(int r, int i){
+        real = r;
+        im = i;
+    }
+
+    comp operator+(comp other){
+        comp result = comp(real + other.real, im + other.im);
+
+        return result;
+    }
+
+    void pretty_print(){
+        cout << real << " + " << im << "i" << endl;
+    }
+};
 class number{
     public:
     int x;
@@ -18,6 +37,12 @@ int main(){
     arr[123];
     cout <<12;
     f();*/
+
+    comp c1 = comp (1, 2);
+    comp c2 = comp (0, 8);
+    comp c3 =  c1 + c2;
+
+    c3.pretty_print();
 
     number a;
     a.x = 4;
