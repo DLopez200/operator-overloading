270bf491bb7bd2b6869e9907b50ad2740b1ed3bc
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-14 20:22:58 -0500

    creating a class of addition

--- overloading.cpp
+++ overloading.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 using namespace std;
 
+class number{
+    public:
+    int x;
 
+    number operator+(number b){
+        number result;
+        result.x = x +b.x;
+        return result;
+    }
+};
 
 int main(){
+    /*Operator:
+    5 + 12;
+    arr[123];
+    cout <<12;
+    f();*/
+
+    number a;
+    a.x = 4;
+    number b;
+    b.x= 12;
+
+    a + b;
+
+    number c = a + b;
+    cout << c.x << endl;
 
     return 0;
 }
